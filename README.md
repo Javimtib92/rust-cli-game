50624ee6bc8177afe05e37f067009bca3cba1e93
Javier Muñoz Tous <javimtib92@gmail.com>
2024-04-24 14:26:58 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Rust CLI Game
+
+This project is a fun project I'm building to play around with Rust while learning.
+
+## Project Goals
+- Learn the fundamentals of Rust programming language.
+- Gain practical experience by implementing various Rust concepts.
+- Explore Rust's unique features such as ownership, lifetimes, and pattern matching.
+- Build a small CLI game with Rust
+
+## Getting Started
+To get started with the Learning Rust Project, follow these steps:
+1. Install Rust: If you haven't already, you'll need to install Rust on your machine. You can download it from [the official Rust website](https://www.rust-lang.org/tools/install).
+2. Clone the Repository: Clone or fork this repository to your local machine using Git.
+3. Explore the Code: Take a look at the existing codebase to familiarize yourself with the project structure.
+4. Start Coding: Begin implementing new features or experimenting with Rust concepts in the project.
+
+## License
+The Learning Rust Project is licensed under the MIT License. See the LICENSE file for more details.
---
+++ src/main.rs
@@ -0,0 +1,91 @@
+use std::{io, thread, time::{Duration, Instant}};
+
+use crossterm::{event::{poll, read, Event, KeyCode}, terminal::{disable_raw_mode, enable_raw_mode}};
+
+const FPS: u32 = 60;
+
+fn main() -> io::Result<()> {
+    println!("Welcome to the Rust CLI game");
+    
+    enable_raw_mode()?;
+
+    let frame_duration = Duration::from_secs(1) / FPS;
+
+    let mut previous_frame_time = Instant::now();
+
+    let mut current_position: Vec<i32> = vec![0, 0];
+    
+    loop {
+        if let Err(e) = update(&mut current_position) {
+            println!("{:?}", e);
+            break;
+        }
+
+        println!("{:?}", current_position);
+
+        paint();
+
+        let now = Instant::now();
+        let elapsed = now - previous_frame_time;
+
+        if elapsed < frame_duration {
+            thread::sleep(frame_duration - elapsed);
+        }
+
+        previous_frame_time = Instant::now();
+    }
+
+    disable_raw_mode()
+}
+
+fn update(position: &mut Vec<i32>) -> io::Result<()> {
+    match read_keyboard_events() {
+        Ok(key_code) => {
+            match key_code {
+                KeyCode::Up => {
+                    position[1] += 1;
+                },
+                KeyCode::Down => {
+                    position[1] -= 1;
+                },
+                KeyCode::Right => {
+                    position[0] += 1;
+                },
+                KeyCode::Left => {
+                    position[0] -= 1;
+                },
+                _ => ()
+            };
+            Ok(())
+        },
+        Err(e) => Err(e)
+    }
+}
+
+fn paint() {
+    
+}
+
+fn read_keyboard_events() -> io::Result<KeyCode> {
+    if poll(Duration::from_millis(500))? {
+        // It's guaranteed that the `read()` won't block when the `poll()`
+        // function returns `true`
+        let event = read()?;
+
+        if event == Event::Key(KeyCode::Esc.into()) {
+            return Err(io::Error::new(io::ErrorKind::Other, "Escape key pressed"));
+        } else if event == Event::Key(KeyCode::Up.into()) {
+            return Ok(KeyCode::Up);
+        } else if event == Event::Key(KeyCode::Down.into()) {
+            return Ok(KeyCode::Down);
+        } else if event == Event::Key(KeyCode::Right.into()) {
+            return Ok(KeyCode::Right);
+        } else if event == Event::Key(KeyCode::Left.into()) {
+            return Ok(KeyCode::Left);
+        }
+    } else {
+        // Timeout expired and no `Event` is available
+    }
+
+    Ok(KeyCode::Null)
+}
