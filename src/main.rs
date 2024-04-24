338c8e9f05b9a38e5a561cb0205d16c11b69c2cf
Javier Muñoz Tous <javimtib92@gmail.com>
2024-04-24 21:36:20 +0200

    implement simple paint function

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use std::{io, thread, time::{Duration, Instant}};
+use std::{io::{self}, thread, time::{Duration, Instant}};
 
-use crossterm::{event::{poll, read, Event, KeyCode}, terminal::{disable_raw_mode, enable_raw_mode}};
+use crossterm::{cursor::MoveTo, event::{poll, read, Event, KeyCode}, execute, style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor}, terminal::{disable_raw_mode, enable_raw_mode, Clear}};
 
 const FPS: u32 = 60;
+const MAP: [i32; 2] = [10, 10];
 
 fn main() -> io::Result<()> {
     println!("Welcome to the Rust CLI game");
@@ -21,7 +22,10 @@ fn main() -> io::Result<()> {
             break;
         }
 
-        paint();
+        if let Err(e) = paint(current_position) {
+            println!("{:?}", e);
+            break;
+        }
 
         let now = Instant::now();
         let elapsed = now - previous_frame_time;
@@ -49,23 +53,45 @@ fn update(position: &mut [i32; 2]) -> io::Result<()> {
 fn update_position(position: &mut [i32; 2], key_code: KeyCode) {
     match key_code {
         KeyCode::Up => {
-            position[1] += 1;
+            position[0] -= 1;
         },
         KeyCode::Down => {
-            position[1] -= 1;
-        },
-        KeyCode::Right => {
             position[0] += 1;
         },
+        KeyCode::Right => {
+            position[1] += 1;
+        },
         KeyCode::Left => {
-            position[0] -= 1;
+            position[1] -= 1;
         },
         _ => ()
     }
 }
 
-fn paint() {
-    
+fn paint(position: [i32; 2]) -> io::Result<()> {
+    let mut map_string = String::new();
+    for row in 0..=MAP[0] {
+        for col in 0..=MAP[1] {
+            if [row, col] == position {
+                map_string.push_str(" • ");
+            } else {
+                map_string.push_str("   ");
+            }
+        }
+        map_string.push_str("\r\n");
+    }
+
+    execute!(
+        io::stdout(),
+        MoveTo(0, 0),
+        Clear(crossterm::terminal::ClearType::All),
+        SetForegroundColor(Color::Rgb { r: 255, g: 203, b: 164 }),
+        SetBackgroundColor(Color::Rgb { r: 19, g: 109, b : 21}),
+        Print(map_string),
+        ResetColor
+    )?;
+
+    Ok(())
 }
 
 fn read_keyboard_events() -> io::Result<KeyCode> {
